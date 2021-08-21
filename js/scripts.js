e41edaf10ab3563e60dc9be6cd8e677961331489
Othman Alomair <most3mr@live.com>
2021-08-22 01:51:05 +0300

    adding Modal markup

--- js/scripts.js
+++ js/scripts.js
@@ -26,10 +26,9 @@ function fetchUsers(url){
 
 fetchUsers('https://randomuser.me/api/?results=12')
     .then(data => {
-        let users = data.results
-        users.forEach(user => {
-            console.log(user)
-            randomUsers.insertAdjacentHTML('beforeend', `
+        const galleryMarkup = data.results
+            .map(user => {
+                return `
                 <div class="card">
                     <div class="card-img-container">
                         <img class="card-img" src="${user.picture.medium}" alt="profile picture">
@@ -40,7 +39,48 @@ fetchUsers('https://randomuser.me/api/?results=12')
                         <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
                     </div>
                 </div>
-            `)
-        })
+            `
+            }).join('')
+            randomUsers.insertAdjacentHTML('beforeend', galleryMarkup)
+
+            //Modal Markup
+            const userCard = document.querySelectorAll('.card')
+            for(let i = 0; i < userCard.length; i++){
+                userCard[i].addEventListener('click',(e) => {
+                    const selectedUser = data.results[i]
+                    modalMarkup(selectedUser)
+                    const modalContainer = document.querySelector('.modal-container')
+                    const closeBtn = document.getElementById('modal-close-btn')
+                    closeBtn.addEventListener('click', () => modalContainer.remove())
+                })
+            }
     })
     
+
+// ------------------------------------------
+//  MODAL MARKUP FUNCTION
+// ------------------------------------------
+
+function modalMarkup(user){
+
+    // formatting Birthday MM/DD/YYYY
+    const dateFormat = new Date(user.dob.date).toLocaleDateString("en-US",{day: '2-digit',month: '2-digit',year: 'numeric'})
+
+    randomUsers.insertAdjacentHTML('beforeend', `
+    <div class="modal-container">
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src="${user.picture.large}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+                <p class="modal-text">${user.email}</p>
+                <p class="modal-text cap">${user.location.city}</p>
+                <hr>
+                <p class="modal-text">${user.phone}</p>
+                <p class="modal-text">${user.location.street.number} ${user.location.street.name}, ${user.location.city}, ${user.location.state} ${user.location.postcode}</p>
+                <p class="modal-text">Birthday: ${dateFormat}</p>
+            </div>
+        </div>
+    </div>
+`)
+}
